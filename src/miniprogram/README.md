904a7fed43ee9aa107fbe62e387ac3b99e1085da
张兴煜 <zhangxingyu1@mail.3-marks.com>
2023-07-06 14:59:16 +0800

    feat: 调试模版

---
+++ src/miniprogram/README.md
@@ -0,0 +1,2 @@
+# sdk-unionpay-miniprogram
+银联小程序sdk对接
--- src/miniprogram/src/UnionMiniService.php
+++ src/miniprogram/src/UnionMiniService.php
@@ -2,8 +2,15 @@
 
 namespace Sangdou\Unionpay\miniprogram\src;
 
-class rService
+class UnionMiniService
 {
+
+
+    public function __construct(...$arguments)
+    {
+        $vars = get_defined_vars();
+    }
+
     public function boot()
     {
         echo '当前时间' . time();
